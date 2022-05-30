f2c1791cb65ee529bbc4ba7b5187cea480b6c979
Sophia Beluli <sophiabeluli@gmail.com>
2022-05-30 15:14:31 -0400

    done!!

--- routes/resume.js
+++ routes/resume.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET resume page. */
 router.get("/", function (req, res, next) {
-    res.sendFile("index.html", {
+    res.sendFile("resume.html", {
         root: "public/html",
     });
 });
--- routes/tigercat.js
+++ routes/tigercat.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
+/* GET resume page. */
 router.get("/", function (req, res, next) {
-    res.sendFile("index.html", {
+    res.sendFile("tigercat.html", {
         root: "public/html",
     });
 });
